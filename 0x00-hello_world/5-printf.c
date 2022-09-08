a114fe65c0fc39a2ac19966a032f936ce89f6ca7
Maryland4 <chike25luba@gmail.com>
2022-09-08 10:17:28 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: 0 number
+ *
+ *     */
+
 int main(void)
+
 {
-	printf("with proper grammer, but the outcome is a piece of art, \n");
-	return (0);
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
