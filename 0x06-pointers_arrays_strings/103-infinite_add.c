9ca957551d749884f5bb14e9f59903e7ddc5ed14
Maryland4 <chike25luba@gmail.com>
2022-09-21 19:43:15 -0700

    infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,12 +1,12 @@
 #include "main.h"
+
 /**
  * infinite_add - adds two numbers
- * @ni: first number
+ * @n1: first number
  * @n2: second number
  * @r: result
  * @size_r: result length
  * Return: sum
- *
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
