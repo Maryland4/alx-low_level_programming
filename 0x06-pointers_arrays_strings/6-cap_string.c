667ecca24359b8062e08931a6761c0305549fd7d
Maryland4 <chike25luba@gmail.com>
2022-09-21 20:02:07 -0700

    capitalize words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,7 +21,7 @@ char *cap_string(char *s)
 			s[0] = s[0] - 32;
 		}
 		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-				|| s[count] == ',' || s[count] == ',' || s[count] == ','
+				|| s[count] == ',' || s[count] == ';' || s[count] == ','
 				|| s[count] == ',' || s[count] == '!' || s[count] == '?'
 				|| s[count] == '"' || s[count] == '(' || s[count] == ')'
 				|| s[count] == '{' || s[count] == '}')
@@ -35,5 +35,3 @@ char *cap_string(char *s)
 	}
 	return (s);
 }
-
-
