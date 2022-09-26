11ab8a93bd2e8a66055d8194c73365aa7cf35939
Maryland4 <chike25luba@gmail.com>
2022-09-26 04:53:20 -0700

    copy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,9 @@
-int _putchar(char c)
+#ifndef FILE_MAIN_H
+#define FILE_MAIN_H
+
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+
+#endif
