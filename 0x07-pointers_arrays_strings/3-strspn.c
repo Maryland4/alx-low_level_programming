ea81c1e7e98662e2f75560ac9882875c82f0822b
Maryland4 <chike25luba@gmail.com>
2022-09-26 05:20:50 -0700

    substring length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,14 +10,14 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	inti, j, f, flag;
+	int i, j, f, flag;
 
 	f = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		flag = 0;
-		for (j = 0; accept[j] != '\0' i++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
